ed0967d62b8f5432ca03f7a19e05387f5d48d4f7
PopoSnek <ariefuddinsatriadharma@gmail.com>
2022-04-22 18:30:37 +0700

    feat : init window

--- Tutorial_1-2/main.cpp
+++ Tutorial_1-2/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 #include "Adder/adder.h"
+#include "GLFW/glfw3.h"
 
 
 int main () {
-     std::cout << "Hello World!" << std::endl;
-     std::cout << add(2 , 3) << std::endl;
-     return 0;
+     GLFWwindow* window;
+     int width, height;
+
+     if( !glfwInit() )
+     {
+         fprintf( stderr, "Failed to initialize GLFW\n" );
+         exit( EXIT_FAILURE );
+     }
+
+     window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Draw gears
+     //    draw();
+
+     //    // Update animation
+     //    animate();
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 }
