6aad0334df39279d266c699dff322827d3bb4291
PopoSnek <ariefuddinsatriadharma@gmail.com>
2022-04-22 16:56:05 +0700

    init

---
+++ Tutorial_1-2/adder/adder.cpp
@@ -0,0 +1,5 @@
+#include "adder.h"
+
+float add(int a, int b) {
+     return a + b;
+} 
---
+++ Tutorial_1-2/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Adder/adder.h"
+
+
+int main () {
+     std::cout << "Hello World!" << std::endl;
+     std::cout << add(2 , 3) << std::endl;
+     return 0;
+}
